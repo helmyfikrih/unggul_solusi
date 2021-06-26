865987b6e7b6a70fdbeef428b88a07b2cc2a9c68
Helmy Fikri Husein <helmyfikrih@gmail.com>
2021-06-26 13:48:59 +0700

    crud data barang

---
+++ application/controllers/Data_barang.php
@@ -0,0 +1,178 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Data_barang extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->plugins_path_css = array();
+		$this->plugins_path_js = array();
+		$this->css_path = array();
+		$this->js_path = array();
+
+        // Model
+        $this->load->model('barang_model', 'barang');
+	}
+
+	public function index()
+	{
+        $this->plugins_path_css = array(
+            'datatables-bs4/css/dataTables.bootstrap4.min.css',
+            'datatables-responsive/css/responsive.bootstrap4.min.css',
+            'sweetalert2-theme-bootstrap-4/bootstrap-4.min.css',
+            'datepicker/css/bootstrap-datepicker.min.css',
+            'select2/css/select2.min.css',
+            'select2-bootstrap4-theme/select2-bootstrap4.min.css',
+        );
+        $this->plugins_path_js = array(
+            'datatables/jquery.dataTables.min.js',
+            'datatables-bs4/js/dataTables.bootstrap4.min.js',
+            'datatables-responsive/js/dataTables.responsive.min.js',
+            'datatables-responsive/js/responsive.bootstrap4.min.js',
+            'sweetalert2/sweetalert2.min.js',
+            'jquery-validation/jquery.validate.min.js',
+            'jquery-validation/additional-methods.min.js',
+            'moment/moment.min.js',
+            'datepicker/js/bootstrap-datepicker.min.js',
+            'select2/js/select2.full.min.js',
+        );
+        $this->js_path = array(
+            'pages/barang.js?x='.time(),
+            'helper/date.js',
+        );
+        $data = array(
+            'header_title' => "Data Pelanggan",
+            'plugins_path_css' => $this->plugins_path_css,
+            'plugins_path_js' => $this->plugins_path_js,
+            'css_path' => $this->css_path,
+            'js_path' => $this->js_path,
+        );
+		$this->template->load('default', 'barang/index', $data);
+	}
+
+	
+    public function getList()
+    {
+		$cond = null;
+        $list = $this->barang->getList($cond);
+        $data = array();
+        $no = $_POST['start'];
+        foreach ($list as $field) {
+            // $no++;
+            $row = array();
+            $btnEdit = "";
+            $btnDelete = "";
+            $btnView = "";
+
+			$btnEdit = '<span><button type="button" class="btn btn-outline-info btn-sm" onclick="edit(\'' . ($field->kode) . '\')"><i class="fa fa-edit"></i> Edit</button></span>';
+			$btnDelete = '<span><button type="button" class="btn btn-outline-danger btn-sm" onclick="deleteData(\'' . ($field->kode) . '\')"><i class="fa fa-trash"></i> Delete</button></span>';
+            $btnView = '<span><button type="button" class="btn btn-outline-success btn-sm" onclick="view(\'' . ($field->kode) . '\')"><i class="fa fa-eye"></i> View</button></span>';
+            $btn = " <div class='d-none d-sm-block d-sm-none d-md-block'>$btnEdit $btnView $btnDelete</div>";
+            $btn .= "   <div class='input-group-prepend d-md-none d-lg-none d-xl-none '>
+                          <button type='button' class='btn btn-default dropdown-toggle dropdown-icon' data-toggle='dropdown'>
+                          </button>
+                          <div class='dropdown-menu'>
+                            <div class='dropdown-item' href='javasctipy:;'>$btnEdit $btnView $btnDelete</div>
+                          </div>
+                        </div>";
+            $row[] = $btn;
+            $row[] = $field->kode;
+            $row[] = $field->nama;
+            $row[] = $field->kategori;
+            $row[] = $field->harga;
+            $data[] = $row;
+        }
+
+        $output = array(
+            "draw"            => $_POST['draw'],
+            "recordsTotal"    => $this->barang->count_new($cond),
+            "recordsFiltered" => $this->barang->count_filtered_new($cond),
+            "data"            => $data,
+        );
+        //output dalam format JSON
+
+        echo json_encode($output);
+    }
+
+	function save()
+    {
+        $kode = $this->input->post('kode');
+        $nama = $this->input->post('nama');
+        $kategori = $this->input->post('kategori');
+        $harga = $this->input->post('harga');
+        $data['pelanggan'] = array(
+            "nama" => $nama,
+            "kategori" => $kategori,
+            "harga" => $harga,
+        );
+        $cond = array(
+            'kode' => $kode
+        );
+        $this->db->trans_begin();
+        if ($kode) {
+			$this->barang->update($data['pelanggan'], $cond);
+        } else {
+			$this->barang->insert($data['pelanggan']);
+        }
+        if ($this->db->trans_status() === FALSE) {
+            $this->db->trans_rollback();
+            $err = $this->db->error();
+            $msg = $err["code"] . "-" . $err["message"];
+            $res = array(
+                'is_success' => false,
+                'message' =>  $msg
+            );
+        } else {
+            $this->db->trans_commit();
+            if ($kode) {
+                $res = array(
+                    'is_success' => true,
+                    'message' => "Berhasil Update Data",
+                );
+            } else {
+                $res = array(
+                    'is_success' => true,
+                    'message' => "Berhasil Menambahkan Data",
+                );
+            }
+        }
+        echo json_encode($res);
+    }
+
+	public function getOne()
+    {
+        $kode = $this->input->post('id');
+		$cond = array(
+			'kode' => $kode
+		);
+        $data = $this->barang->getOne($cond);
+        echo json_encode($data);
+    }
+
+	function delete()
+    {
+
+
+        $kode = $this->input->post('uid');
+
+        $cond = array(
+            "kode" => $kode,
+        );
+
+        if ($this->barang->delete($cond)) {
+            $res = array(
+                'is_success' => true,
+                'message' => "Berhasil Menghapus Data Pelanggan",
+            );
+        } else {
+            $err = $this->db->error();
+            $msg = $err["code"] . "-" . $err["message"];
+            $res = array(
+                'is_success' => false,
+                'message' =>  $err
+            );
+        }
+        echo json_encode($res);
+    }
+}
--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -1,86 +1,135 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Starter Page</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+	<!-- Content Header (Page header) -->
+	<div class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1 class="m-0 text-dark">Data Barang</h1>
+				</div><!-- /.col -->
+				<div class="col-sm-6">
+					<ol class="breadcrumb float-sm-right">
+						<li class="breadcrumb-item"><a href="#">Home</a></li>
+						<li class="breadcrumb-item active">Data Barang</li>
+					</ol>
+				</div><!-- /.col -->
+			</div><!-- /.row -->
+		</div><!-- /.container-fluid -->
+	</div>
+	<!-- /.content-header -->
+	<!-- Main content -->
+	<div class="content">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-12">
+					<div class="card">
+						<div class="card-header">
+							<h3 class="card-title text-left">Data Barang</h3>
+							<div class="text-right">
+								<button type="button" class="btn btn-sm btn-primary" onclick="addNew()">
+									<i class="fas fa-plus"></i> Add New
+								</button>
+							</div>
+						</div>
+						<!-- /.card-header -->
+						<div class="card-body">
+							<table id="datatables" class="table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th>Action</th>
+										<th>Kode</th>
+										<th>Nama</th>
+										<th>Kategori</th>
+										<th>Harga</th>
+									</tr>
+								</thead>
+							</table>
+						</div>
+						<!-- /.card-body -->
+					</div>
+				</div>
+			</div>
+			<!-- /.row -->
+		</div><!-- /.container-fluid -->
+	</div>
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+	<!-- Modals -->
+	<div class="modal fade" id="modalAddEdit" data-backdrop="static" data-keyboard="false" role="dialog" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-scrollable modal-xl">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title"><span class="modalHeaderText"></span></h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body" id="modalBody">
+					<form role="form" id="form" action="<?= base_url('data_barang/save') ?>">
+						<div class="row">
+							<div class="col-sm-6">
+								<!-- text input -->
+								<div class="form-group">
+									<label>Nama</label>
+									<input type="text" name="kode" id="kode" class="form-control" placeholder="kode" hidden>
+									<input type="text" name="nama" id="nama" class="form-control" placeholder="Nama">
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="form-group">
+									<label>Kategori</label>
+									<select name="kategori" id="kategori" class="form-control select2bs4 select-form" style="width: 100%;">
+										<option></option>
+										<option value="ATK">ATK</option>
+										<option value="RT">RT</option>
+										<option value="MASAK">Masak</option>
+										<option value="ELEKTRONIK">Elektronik</option>
+									</select>
+								</div>
 
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the card's
-                                content.
-                            </p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-6">
+								<div class="form-group">
+									<label>Harga</label>
+									<input type="number" min="0" name="harga" id="harga" class="form-control" placeholder="Harga">
+								</div>
+							</div>
+						</div>
+				</div>
+				<div class="modal-footer justify-content-between">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-primary">Save</button>
+				</div>
+				</form>
+			</div>
+			<!-- /.modal-content -->
+		</div>
+		<!-- /.modal-dialog -->
+	</div>
+	<div class="modal fade" id="modalView" data-backdrop="static" data-keyboard="false" role="dialog" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-scrollable modal-xl">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title"><span class="modalHeaderText"></span></h4>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body" id="modalBody">
+					<div id="body-view">
 
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                    </div>
-
-                    <div class="card card-primary card-outline">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-
-                            <p class="card-text">
-                                Some quick example text to build on the card title and make up the bulk of the card's
-                                content.
-                            </p>
-                            <a href="#" class="card-link">Card link</a>
-                            <a href="#" class="card-link">Another link</a>
-                        </div>
-                    </div><!-- /.card -->
-                </div>
-                <!-- /.col-md-6 -->
-                <div class="col-lg-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-
-                    <div class="card card-primary card-outline">
-                        <div class="card-header">
-                            <h5 class="m-0">Featured</h5>
-                        </div>
-                        <div class="card-body">
-                            <h6 class="card-title">Special title treatment</h6>
-
-                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <!-- /.col-md-6 -->
-            </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
+					</div>
+				</div>
+				<div class="modal-footer justify-content-between">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+			<!-- /.modal-content -->
+		</div>
+		<!-- /.modal-dialog -->
+	</div>
+	<!-- /.modal -->
+	<!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
